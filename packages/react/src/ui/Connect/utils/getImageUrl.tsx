2e44d5a11c02b2d5fe3e8360a4c4c58ae204bba3
Nelito Junior <ngvjqp@gmail.com>
2025-06-04 23:03:11 -0300

    feat:dark (#30)

--- packages/react/src/ui/Connect/utils/getImageUrl.tsx
+++ packages/react/src/ui/Connect/utils/getImageUrl.tsx
@@ -3,7 +3,7 @@ import type { ConnectorMetadata } from 'fuels';
 export const getImageUrl = (connector: ConnectorMetadata, theme?: string) => {
   const { image } = connector;
   if (typeof image === 'object') {
-    return theme === 'dark' ? image.dark : image.light;
+    return theme !== 'dark' ? image.light : image.dark;
   }
   return image;
 };
